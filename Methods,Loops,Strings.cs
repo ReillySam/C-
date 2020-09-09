693652ef76e0f4ed1c23a599c314e1b0269bd44c
Sam Reilly <samreilly132@icloud.com>
2020-09-09 20:58:26 +0200

    Update Methods,Loops,Strings.cs

--- Methods,Loops,Strings.cs
+++ Methods,Loops,Strings.cs
@@ -57,25 +57,6 @@ class Program
         Console.WriteLine("Enter a string: ");
         string string_1 = Console.ReadLine();
 
-        // Console.WriteLine(string_1.Replace(string_1[0], string_1[^1]));
-
-        for (var i = 1; i < string_1.Length; i++)
-        {
-            if (string_1[i] == "a")
-            {
-                string_1.Replace("a", "b");
-            }
-            else if (string_1[i] == "b")
-            {
-                string_1.Replace("b", "a");
-            }
-            else
-            {
-                continue;
-            }
-
-        }
-        Console.WriteLine(string_1);
-
+        Console.WriteLine(string_1.Replace(string_1[0], string_1[^1]), string_1.Replace(string_1[^1], string_1[0]));
     }
 }
